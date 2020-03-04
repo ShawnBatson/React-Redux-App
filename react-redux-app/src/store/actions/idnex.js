b5539f5f745f343a3a481d528634b1422c706510
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-03-03 18:56:59 -0600

    added file structure, api get, and action list

---
+++ react-redux-app/src/store/actions/idnex.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const FETCHING_SPELLS_START = "FETCHING_SPELLS_START";
+export const FETCHING_SPELLS_SUCCESS = "FETCHING_SPELLS_SUCCESS";
+export const FETCHING_SPELLS_FAILURE = "FETCHING_SPELLS_FAILURE";
+
+export const getSpell = () => dispatch => {
+  dispatch({ type: FETCHING_SPELLS_START });
+
+  axios
+    .get(`http://dnd5eapi.co/api/spells/${index}`)
+    .then(res => {
+      console.log("res", res);
+      dispatch({ type: FETCHING_SPELLS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log("err", err);
+      dispatch({
+        type: FETCHING_QUOTE_FAILURE,
+        payload: `${err.response.message} with response code ${err.response.code}`
+      });
+    });
+};
